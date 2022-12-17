d7d971f9c8892f46acacbc975e135e2ed189716f
Eryiskerh <viktoriya.alyaseva@gmail.com>
2022-12-18 01:59:38 +0300

    Integration tests for F(x)

--- lab78/tests/UnitTest1.cs
+++ lab78/tests/UnitTest1.cs
@@ -1,10 +1,38 @@
 namespace tests;
+using proj;
 
 [TestClass]
-public class UnitTest1
+public class TestApp
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestF_1()
     {
+		Assert.AreEqual(App.F(0d), -1d);
     }
+	[TestMethod]
+    public void TestF_2()
+    {
+		Assert.AreEqual(App.F(1d), -6.84875379306251d);
+    }
+	[TestMethod]
+    public void TestF_3()
+    {
+		Assert.AreEqual(App.F(-1d), -0.6756375891047525d);
+    }
+	[TestMethod]
+    public void TestF_4()
+    {
+		Assert.AreEqual(App.F(-0.5d), -0.47757726140773893d);
+    }
+	[TestMethod]
+    public void TestF_5()
+    {
+		Assert.AreEqual(App.F(0.5d), -2.6085840082227483d);
+    }
+	[TestMethod]
+    public void TestF_6()
+    {
+		Assert.AreEqual(App.F(double.MaxValue), -1d*double.MaxValue*double.MaxValue);
+    }
+	
 }
